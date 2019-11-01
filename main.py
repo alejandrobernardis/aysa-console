051c733150ec61635d8651384415c6c442bca6b9
Alejandro M. BERNARDIS <alejandro.bernardis@gmail.com>
2019-11-01 15:48:56 -0300

    UPD // +Fixes

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import sys
 import logging
 from fabric import Connection
 from prompt_toolkit import PromptSession
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import FileHistory
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.shortcuts import yes_no_dialog
+from prompt_toolkit.styles import Style
 from docopt import docopt, DocoptExit
 from inspect import getdoc
 
@@ -30,39 +34,59 @@ ENDPOINTS = {
 }
 
 # objects
+bindings = KeyBindings()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-class Commands:
-    """
-    AySA Command Line Interface.
-
-    Usage: COMMAND [ARGS...]
-
-    Comandos Disponibles:
-        exit      Termina el proceso.
-        help      Muestra la ayuda.
-        remote    Ejecuta comando de forma remota.
-    """
-
-    def __init__(self, endpoints, cnx_create=False, **kwargs):
+class BaseCommands:
+    def __init__(self, endpoints, cnx_create=False, default='development',
+                 **kwargs):
         self.__cnx = {}
+        self.__endpoint = None
         self.__endpoints = endpoints
+        self.__env = kwargs.pop('env', None)
         self.__options = kwargs
 
         if cnx_create is True:
             for endpoint in self.__endpoints.keys():
-                self._cnx(endpoint)
+                self.get_cnx(endpoint)
 
-    def _cnx(self, endpoint):
+        self.set_env(default)
+
+    @property
+    def env(self):
+        return self.__env
+
+    @property
+    def endpoint(self):
+        return self.__endpoint
+
+    def set_env(self, value):
+        value = str(value).lower()
+        if value not in self.__endpoints:
+            raise KeyError('El endpoint "{}" no es válido.'.format(value))
+        self.__env = self.get_cnx(value)
+        self.__endpoint = value
+        log.info('endpoint (set): "%s"', value)
+
+    def get_cnx(self, endpoint):
         try:
-            return self.__cnx[endpoint]
+            cnx = self.__cnx[endpoint]
+            log.info('endpoint (cache): "%s"', endpoint)
         except KeyError:
-            log.debug('endpoint key: "%s"', endpoint)
+            log.debug('endpoint (create): "%s"', endpoint)
             cnx = Connection(**self.__endpoints[endpoint])
             self.__cnx[endpoint] = cnx
-            return cnx
+        return cnx
+
+    def get_yes(self, title=None, text=None, **kwargs):
+        return kwargs.get('--yes', False) or \
+               yes_no_dialog(title=title or 'ATENCIÓN!',
+                             text=text or 'Desea continuar?')
+
+    def get_doc(self, obj=None):
+        return ' \n{}\n\n '.format(getdoc(obj or self))
 
     def parse(self, line, *args, **kwargs):
 
@@ -80,13 +104,13 @@ class Commands:
         cmd = values[0]
         arg = values[1:]
 
-        log.debug('cmd: %s, arg: %s', cmd, arg)
+        log.debug('cmd (%s): %s', cmd, arg)
 
         if not hasattr(self, cmd):
             return self.help()
 
         hdr = getattr(self, cmd)
-        doc = getdoc(hdr)
+        doc = self.get_doc(hdr)
 
         try:
             opt = docopt(doc, arg, **kwargs)
@@ -94,26 +118,54 @@ class Commands:
         except DocoptExit:
             log.error(doc)
 
-    def remote(self, options, *args, **kwargs):
-        """
-        Ejecuta comando de forma remota.
-
-        Usage: remote [options] CMD [ARGS...]
-
-        Opciones:
-            -y, --yes    Responde "SI" a todas las preguntas.
-
-        """
-        print(options)
-
-    def help(self):
-        log.error(getdoc(Commands))
+    def help(self, *args, **kwargs):
+        print(self.get_doc())
 
     def exit(self, code=0):
         sys.exit(code)
 
 
+class Commands(BaseCommands):
+    """
+    AySA Command Line Interface.
+
+    Usage: COMMAND [ARGS...]
+
+    Comandos Disponibles:
+        deploy     Inicia el proceso de despliegue.
+        down       Detiene y elimina todos servicios.
+        prune      Detiene y elimina todos servicios, omo así también
+                   las imágenes y volúmenes aosicados.
+        restart    Reinicia uno o más servicios.
+        services   Lista los servicios disponibles.
+        set        Estable el entorno a utilizar [default: development].
+        start      Inicia uno o más servicios.
+        stop       Detiene uno o más servicios.
+        up         Crea e inicia uno o más servicios.
+
+    Comando Generales:
+        help       Muestra la ayuda del programa.
+        exit       Sale del programa.
+    """
+    def deploy(self, options, **kwargs):
+        """
+        usage: deploy [options] [ARGS...]
+
+        Argumentos Opcionales:
+            -y, --yes    Responde "SI" a todas las preguntas.
+        """
+        if self.get_yes(**options):
+            log.info(options)
+
+    def set(self, options, **kwargs):
+        """
+        usage: set ENVIRONMENT
+        """
+        self.set_env(options['ENVIRONMENT'])
+
+
 def main():
+    # argmuents
     argv = sys.argv[1:]
 
     # logger
@@ -127,27 +179,38 @@ def main():
     session = PromptSession(
         completer=WordCompleter([
             'help',
-            'remote',
             'exit',
+            'deploy',
+            'set',
             'development',
             'quality'
         ]),
         history=FileHistory('c:/users/i0608156/.aysa/history'),
-        mouse_support=True
+        auto_suggest=AutoSuggestFromHistory(),
+        mouse_support=True,
+        key_bindings=bindings
     )
 
     # dispatcher
     commands = Commands(ENDPOINTS)
 
+    # styles
+    style = Style.from_dict({
+        '': '#FFFFFF',
+        'environ': '#006600'
+    })
+
     while 1:
         try:
-            value = session.prompt('> ')
+            ps1 = [
+                ('class:environ', '({}) '.format(commands.endpoint)),
+                ('class:', '> ')
+            ]
+            commands.parse(session.prompt(ps1, style=style))
         except KeyboardInterrupt:
             continue
         except EOFError:
             break
-        else:
-            commands.parse(value)
 
 
 if __name__ == '__main__':
